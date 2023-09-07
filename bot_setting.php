05f4b7ae7c5d64b0d80cee4e3b158c0589dc0ea4
DewThanathon <144294682+DewThanathon@users.noreply.github.com>
2023-09-07 08:08:10 +0700

    Create bot_setting.php

---
+++ bot_setting.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1657218294');
+define('LINE_MESSAGE_CHANNEL_SECRET','7f8d309cf043d22a3743aaf974bcd536');
+define('LINE_MESSAGE_ACCESS_TOKEN','s/uxtrabzaK6Bth6NKWfq373czQDrlHity5HF5+osqv9ls4NP/g4KX+0GBwbQlAO80PmrU0R7gR+kh7WFfK24Gk+n+MVEaliSfPQBa72bAPhSsyV8e+9oqboiiiKScPji/IQ7IzkeM4EAoze2kDGTAdB04t89/1O/w1cDnyilFU=');
+?>
